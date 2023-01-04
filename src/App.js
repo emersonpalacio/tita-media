d1a0adb1c38b4271d3d2874fd759a321652037a0
emersonpalacio <emersonpalaciootalvaro@gmail.com>
2023-01-03 20:57:46 -0500

    login-google

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { useState, useEffect } from "react";
+import { gapi } from "gapi-script";
+import GoogleLogin from "react-google-login";
+
 function App() {
+  const clientID =
+    "152246615921-plavoo3cte9rc53n7pdfn9p7lbgm2oa1.apps.googleusercontent.com";
+  const [user, setUser] = useState({});
+
+  const onSuccess = (response) => {
+    setUser(response.profileObj);
+    document.getElementsByClassName("btn").hidden = true;
+  };
+  const onFailure = (response) => {
+    console.log("Something went wrong");
+  };
+
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId: clientID,
+      });
+    }
+    gapi.load("client:auth2", start);
+  });
+
   return (
-    <div>
-      <h1>Tita-Media</h1>
+    <div className="center">
+      <div class={user ? "profile" : "hidden"}>
+        <h3>{user.name}</h3>
+      </div>
+
+      <div className="btn">
+        <GoogleLogin
+          clientId={clientID}
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+          buttonText="Continue  with Google"
+          cookiePolicy={"single_host_origin"}
+        />
+      </div>
     </div>
   );
 }
